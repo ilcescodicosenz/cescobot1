8d7eadf3d3718242b8ba71b78e52060789385657
ilcescodicosenz <cescokek@gmail.com>
2025-02-28 22:31:19 +0100

    Update info-owner.js

--- plugin/info-owner.js
+++ plugin/info-owner.js
@@ -8,14 +8,16 @@ function handler(m) {
         'message': {
             'extendedTextMessage': {
                 'text': 'Proprietario del bot',
-                'vcard': 'BEGIN:VCARD\nVERSION:1.0\nN:;Unlimited;;;\nFN:Unlimited\nORG:Unlimited\nTITLE:\nitem1.TEL;waid=19709001746:+39 375 585 3799\nitem1.X-ABLabel:Unlimited\nX-WA-BIZ-DESCRIPTION:ofc\nX-WA-BIZ-NAME:Unlimited\nEND:VCARD'
+                'vcard': 'BEGIN:VCARD\nVERSION:1.0\nN:;Unlimited;;;\nFN:Unlimited\nORG:Unlimited\nTITLE:\nitem1.TEL;waid=393755853799:+39 375 585 3799\nitem1.X-ABLabel:Unlimited\nX-WA-BIZ-DESCRIPTION:ofc\nX-WA-BIZ-NAME:Unlimited\nEND:VCARD'
             }
         },
         'participant': '0@s.whatsapp.net'
     };
     
     const ownerList = global.owner.filter(([number, isOwner]) => number && isOwner);
-    this.sendContact(m.chat, ownerList.map(([number, isOwner]) => [number, isOwner]), contactInfo);
+    // Aggiungiamo il numero 393755853799 direttamente alla lista degli owner
+    ownerList.push([ '393755853799@c.us', true ]);
+    this.sendContact(m.chat, ownerList.map(([number, isOwner]) => [number]), contactInfo);
 }
 
 handler.help = ['proprietario'];
